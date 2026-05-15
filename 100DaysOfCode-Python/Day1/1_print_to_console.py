7ed388ca3463ff507f584ea7d316376e20ddbf98
Vishal <sia052402@gmail.com>
2026-05-15 07:35:03 +0530

    Day1 : printing statements in Python

---
+++ 100DaysOfCode-Python/Day1/1_print_to_console.py
@@ -0,0 +1,6 @@
+print("Hello World!")
+# this is a simple program which instructs computer to print "Hello World!" on console.
+# double quotes are used to define a string in python. We can also use single quotes to define a string.
+print('Hello World!')
+# we can also use triple quotes to define a string in python. Triple quotes are used to define multi-line strings in python.
+print("""Hello World!""")
