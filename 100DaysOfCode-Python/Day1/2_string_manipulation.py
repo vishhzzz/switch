73a20dd5164b1bbc296011e19545783d06e66570
Vishal <sia052402@gmail.com>
2026-05-16 11:49:33 +0530

    Day1: string manipulation

---
+++ 100DaysOfCode-Python/Day1/2_string_manipulation.py
@@ -0,0 +1,15 @@
+print("Hello World!")
+
+# In order to print this on multiple lines then we can use this:
+print("Hello World!\nHello World!")
+# we can also use triple quotes to define a string in python. Triple quotes are used to define multi-line strings in python.
+print("""Hello World!
+Hello World!""")
+
+# String concatenation
+print("Hello" + "Vishal!")
+
+# adding space in between
+print("Hello" + " " + "Vishal!")
+print("Hello " + "Vishal!")
+print("Hello" + " Vishal!")
