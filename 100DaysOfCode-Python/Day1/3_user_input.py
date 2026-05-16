ca29227910f3ed9484b634b8a0733d8a3e852930
Vishal <sia052402@gmail.com>
2026-05-16 15:53:56 +0530

    Day 1: user inputs

---
+++ 100DaysOfCode-Python/Day1/3_user_input.py
@@ -0,0 +1,9 @@
+print("Hello World!")
+
+# taking user input
+input("Enter your name:") #we r not capturing the input rn.
+
+# capturing user input
+print("Hello " + input("Enter your name: "))
+
+print("Hello " + input("Enter your name: ") + "!")
