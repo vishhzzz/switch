dec8719533ea1604e8412282d16f8e0d571bd46f
Vishal <sia052402@gmail.com>
2026-05-19 22:20:12 +0530

    Day1: variables +  Band Name Generator

---
+++ 100DaysOfCode-Python/Day1/4_variable.py
@@ -0,0 +1,6 @@
+# instead of doing everything in one line, we can store the value in a variable and then use it later in the program.
+
+print("Hello " + input("Enter your name: "))
+
+name = input("Enter your name: ")
+print("Hello " + name + "!")
---
+++ 100DaysOfCode-Python/Day1/6_Day1_project.py
@@ -0,0 +1,9 @@
+# band name generator
+print("Welcome to the Band Name Generator.")
+print("What's the name of the city you grew up in?")
+city = input()
+
+print("What's your pet's name?")
+pet = input()
+
+print("Your band name could be " + "\"" + city + " " + pet + "\"")
