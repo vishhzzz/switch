5f141cd131f1b003bb2120868797cb920ef174a4
Vishal <sia052402@gmail.com>
2026-05-20 22:06:28 +0530

    Day2: primitive Data types

---
+++ 100DaysOfCode-Python/Day2/1_primitiveDataTypes.py
@@ -0,0 +1,19 @@
+# print(len(12234)) # error : int obj has no len() function.
+
+# Subsrcipting
+print("Hello"[0]) # print 0th char
+print("Hello"[-1]) #print last char
+
+print("123" + "345") # concatenation of strings
+
+print(123 + 345) # addition of integers
+
+print(123_456_789) # we can use underscore to make the number more readable. It will not affect the value of the number. It is just for readability. 
+# Similar to 123,456,789
+
+# Float
+print(3.14) # float value
+
+# Boolean
+print(True) # boolean value
+print(False) # boolean value    
