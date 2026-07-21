976e5d5cdb9188d7a07b6c0cc473285fd8f834ef
Vishal <sia052402@gmail.com>
2026-07-21 23:54:23 +0530

    angela's ist help video for blackjack project

---
+++ 100DaysOfCode-Python/Day10-CapstoneProject/angela_way/blackjack1.py
@@ -0,0 +1,16 @@
+import random
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+def deal_card():
+    """Docstring : This is the function to provide the card each time either computer or player hits."""
+    card = random.choice(cards)
+    return card
+
+player_cards = []
+computer_cards = []
+
+# intial 2 cards
+for _ in range(2): # we r using _ becoz here we dont need any variable of any kind.
+    player_cards.append(deal_card)
+    computer_cards.append(deal_card)
