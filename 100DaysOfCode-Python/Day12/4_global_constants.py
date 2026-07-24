8a3e9b50b521c705e86bc925e9c52cb7e48ba83d
Vishal <sia052402@gmail.com>
2026-07-24 22:26:05 +0530

    [DAY 12] GLOBAL CONSTANTS

---
+++ 100DaysOfCode-Python/Day12/4_global_constants.py
@@ -0,0 +1,6 @@
+# suppose u wanna define certain variables which are "global" but also constant at same time.
+# like pi or something similar
+
+PI = 3.14
+
+# now it is considered as "GLOBAL CONSTANTS" these r globally and at same time constant which we cant change but yes use also.
