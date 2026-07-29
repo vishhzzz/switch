df4d914258e22310b3dd145f75a3d13f52b4bbf5
Vishal <sia052402@gmail.com>
2026-07-30 01:05:50 +0530

    [DAY 16] oops coffee - complete

--- 100DaysOfCode-Python/Day16/3_coffee_machine_oops.py
+++ 100DaysOfCode-Python/Day16/3_coffee_machine_oops.py
@@ -9,12 +9,17 @@ coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
 
 while True:
-    user_choice = "What would you like? ({menu.get_items()}): "
+    user_choice = input(f"What would you like? ({menu.get_items()}): ")
     if user_choice == 'off':
         print("Machine is being turned off for maintenance")
         break
     elif user_choice == 'report':
         coffee_maker.report() #will print the report of coffee machine
-    elif user_choice == 'cgh':
-        # we need to use find_drink
-        menu.find_drink('espresso')
+        money_machine.report()
+    else:
+        drink = menu.find_drink(user_choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            # we now need to process payments...
+            if money_machine.make_payment(drink.cost):
+                # make coffee
+                coffee_maker.make_coffee(drink)
