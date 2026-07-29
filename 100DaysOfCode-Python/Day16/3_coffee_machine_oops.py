bcdeaee2bdadc55356c98215ed0feaa34a236d54
Vishal <sia052402@gmail.com>
2026-07-29 23:32:18 +0530

    [DAY 16] oops coffee - find drink

--- 100DaysOfCode-Python/Day16/3_coffee_machine_oops.py
+++ 100DaysOfCode-Python/Day16/3_coffee_machine_oops.py
@@ -1,5 +1,20 @@
 from menu import Menu
+from menu import MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-# will continue tomorrow
+# menu is an object from Menu class
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+while True:
+    user_choice = "What would you like? ({menu.get_items()}): "
+    if user_choice == 'off':
+        print("Machine is being turned off for maintenance")
+        break
+    elif user_choice == 'report':
+        coffee_maker.report() #will print the report of coffee machine
+    elif user_choice == 'cgh':
+        # we need to use find_drink
+        menu.find_drink('espresso')
