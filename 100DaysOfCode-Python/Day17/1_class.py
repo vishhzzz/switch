d3f1cdf912573ee5fba8c408b65c634ddd0faf1c
Vishal <sia052402@gmail.com>
2026-07-30 23:47:25 +0530

    [DAY 17] classes basics

---
+++ 100DaysOfCode-Python/Day17/1_class.py
@@ -0,0 +1,19 @@
+# Class is just a blueprint for creating an object.
+
+# Class creation
+# class class_name:
+    # rest will be indented as follows
+
+
+class User:
+    pass # this is a keyword to create empty classes or functions.
+
+user = User()
+
+# Naming Convention: Name of every letter in class must be capital.
+#  This is called as Pascal Case.
+#  Very similar to it is Camel Case the only diff is:
+# VishalKumar <-- Pascal Case
+# vishalKumar <-- Camel Case
+
+# There is something known as Snake Case : vishal_kumar
