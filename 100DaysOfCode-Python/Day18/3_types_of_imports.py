40d63263328b8f415b775b746da9559a2eeb0a76
Vishal <sia052402@gmail.com>
2026-08-02 15:29:36 +0530

    [DAY 18] imports types

---
+++ 100DaysOfCode-Python/Day18/3_types_of_imports.py
@@ -0,0 +1,28 @@
+# There r diff. types of imports:
+# 1. Basic Import:
+            # keyword module_name
+            # import turtle
+            # turtle = turtle.Turtle()
+
+# 2. from ... import ...:
+            # keyword module_name keyword thing_in_module
+            # from turtle import Turtle
+            # turtle = Turtle()
+
+# 3. from ... import *:
+            # same as above but only instead of 1 thing, import all.
+# ---> we can use everything from that module as if it is in my current file 
+# ---> this may confuse 'from where does it came from'
+
+# 4. aliasing modules:
+            # keyword module_name keyword alias_name
+            # import turtle as t:
+
+# 5. installing modules:
+            # for those whom we cant just import.
+
+            # importing those modules which r not shipped with python.
+            # we need to ist install that and then use it.
+            # we can install them from python package maintainer i.e., pypi
+
+
