08d40840f84c233ad5509be0c178cea1bcdd3d95
Vishal <sia052402@gmail.com>
2026-08-03 21:25:01 +0530

    [DAY19] object instance and state

---
+++ 100DaysOfCode-Python/Day19-instances_state_and_higher_order_functions/3_object_state_and instances.py
@@ -0,0 +1,24 @@
+# What if we require multiple turtles at same time.???
+# and how do we get hold of them.?
+
+
+# We can create as many as we want in same manner as we were doing all along.
+from turtle import Turtle
+
+
+tim = Turtle()
+tommy = Turtle()
+tom = Turtle()
+jerry = Turtle()
+
+# these all the the turtle objects created from Turtle class.
+# Technically these r each a seperate instance i.e., an example of turtle object.
+
+
+# STATE
+# these instance can have diff. attributes and can be performing diff. methods at any one time.
+# this is called as STATE or precisely object's state.
+
+
+# Basically multiple objects r called as Instance
+# State: at any time, diff objects can be doing diff things such as using a variable or runing a function --- their state can be diff.
