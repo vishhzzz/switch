9cdedd961c083ffad45cf7793e808f28c8d834b0
Vishal <sia052402@gmail.com>
2026-08-05 08:07:25 +0530

    [Day20] snake requirement

---
+++ 100DaysOfCode-Python/1_requirements.py
@@ -0,0 +1,12 @@
+'''
+Snake Game
+
+We will do this project in several steps:
+1. Create a snake body.
+2. Move the snake.
+3. Control the snake.
+4. Detect collision with food.
+5. Create scoreboard.
+6. detect collision with wall.
+7. detect collision with tail.
+'''
