4f2575fc1d08399d1b38ca36ca61e8368971fab7
Vishal <sia052402@gmail.com>
2026-08-15 17:14:27 +0530

    [DAY24] file path

---
+++ 100DaysOfCode-Python/Day24-Files_Directories_PATH/3_types_of_files.py
@@ -0,0 +1,10 @@
+# root folder in mac or any os is simply represented as '/'.
+
+# / means root.
+
+# there r 2 types of path:
+# 1. absolute path: always start relative with root. Thus start with '/'.
+# 2. relative path: always start wrt to file u r working on or with. 
+# ./ means look into this current folder.
+
+
