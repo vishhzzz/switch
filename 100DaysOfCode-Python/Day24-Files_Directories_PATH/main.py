6dc455918b0ca83dfaf05790ca458cc57d1057da
Vishal <sia052402@gmail.com>
2026-08-15 15:04:16 +0530

    [DAY24] snake game with high score

---
+++ 100DaysOfCode-Python/Day24-Files_Directories_PATH/main.py
@@ -0,0 +1,60 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+def stop_game():
+    global game_is_on
+    game_is_on = False
+    scoreboard.end_game()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+screen.onkey(stop_game, "space")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        # game_is_on = False
+        snake.reset()
+        scoreboard.reset_game()
+
+    #Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            # game_is_on = False
+            scoreboard.reset_game()
+            snake.reset()
+
+
+
+
+
+screen.exitonclick()
---
+++ 100DaysOfCode-Python/Day24-Files_Directories_PATH/scoreboard.py
@@ -0,0 +1,37 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.high_score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    # def game_over(self): # we dont need this anymore, we need to reset the game.
+    def reset_game(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        # self.clear()
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 1
+        # self.clear() #right now we r using it at 2 places here and in reset game so instead we can write this at one place and remove from others.
+        self.update_scoreboard()
+
+    def end_game(self):
+        self.goto(0, 0)
+        self.write("Game Over", align=ALIGNMENT, font=FONT)
