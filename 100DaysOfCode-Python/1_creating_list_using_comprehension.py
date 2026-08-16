4c512d903fe4a16c23f77fcd4e268df9353a7c48
Vishal <sia052402@gmail.com>
2026-08-17 01:25:07 +0530

    [DAY26] list comprehension

---
+++ 100DaysOfCode-Python/1_creating_list_using_comprehension.py
@@ -0,0 +1,35 @@
+# Unique to Python
+# it increases readablity and decreases no. of code lines.
+
+# Comprehension can applied on any sequence:
+# list
+# tuple
+# string
+# range
+
+# List Comprehension: creates a new list from old list.
+
+# Old Way
+no_list = [1, 2, 3]
+new_list = []
+
+for ele in no_list:
+    new_ele = ele + 1
+    new_list.append(new_ele) #2, 3, 4
+
+
+# MENTOS way / Comprehension
+# new_list = [new_item for item in list]
+new_list = [ele + 1 for ele in no_list ]
+print(new_list)
+
+# Comprehension works for any sequence
+# like - string
+name = "vishal"
+new_list = [letter for letter in name]
+print(new_list) #--> ['v', 'i', 's', 'h', 'a', 'l']
+
+
+# Comprehension for range
+new_list = [ele * 2 for ele in range(1, 5)]
+print(new_list)
