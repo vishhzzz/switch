5bf0d79ff6a2293fc55271f78183ad7f8304211f
Vishal <sia052402@gmail.com>
2026-08-22 14:02:14 +0530

    [DAY28] working with image-canvas

---
+++ 100DaysOfCode-Python/DAY28-Pomodoro/1_canvas_widget_adding_images_in_tkinter.py
@@ -0,0 +1,46 @@
+# Canvas widget of tkinter is responsible for adding images on ui.
+# allows to layer things on top of others.
+
+from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+# 8800483748
+# ------------------------------ TIMER RESET ---------------------------------- # 
+
+# ----------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# -------------------------------- UI SETUP ----------------------------------- #
+# window
+window = Tk()
+window.title("Pomodoro")
+# we r just seeing tomato image.
+window.config(padx=100, pady=50, bg=YELLOW)
+
+
+# canvas
+canvas = Canvas()
+
+# we can set this in constructor also and can later modify too.
+# highlightthickness is to remove border across the image.
+canvas.config(width=200, height=224, bg=YELLOW, highlightthickness=0)
+
+# canvas requires photo in form of photo image not as path to image.
+img = PhotoImage(file='./DAY28-Pomodoro/tomato.png')
+
+# we have devloped the screen of canvas, now its time to add image on top of it.
+canvas.create_image(100, 112, image=img)
+canvas.pack()
+
+# adding text on top of image
+canvas.create_text(100, 130, text="00:00", fill='white', font=(FONT_NAME, 35, 'bold'))
+
+window.mainloop()
