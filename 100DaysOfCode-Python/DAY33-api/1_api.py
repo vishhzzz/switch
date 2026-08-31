46fa243ba3f2cc90d20a335ff969cd9a432a04f7
Vishal <sia052402@gmail.com>
2026-08-31 21:19:55 +0530

    [DAY33] api

---
+++ 100DaysOfCode-Python/DAY33-api/1_api.py
@@ -0,0 +1,18 @@
+'''
+API - Application Programming Interface
+
+It is a set of commands, functions, protocols and objects that programmers can use to create s/w or interact with an external system.
+
+API is more likely an interface or more accurately barrier between my system and external system.
+What we r essentially doing:
+1. use those rules set by API and make a request to the external system for some piece of data.
+2. if the request has been structured according to rules/requirements set by external system via api then they will respond appropriately and give data which u demanded.
+
+
+Analogy:
+
+My system which requests an API         --- TUMMY
+External System where i want to tap to  --- RESTRAUNT
+API                                     --- MENU
+
+'''
