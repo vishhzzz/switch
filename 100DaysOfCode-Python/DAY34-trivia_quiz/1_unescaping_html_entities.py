adb29941865a7b16203c4c2b3ba70ad1f29ae9e7
Vishal <sia052402@gmail.com>
2026-09-04 13:19:16 +0530

    [DAY34] unescaping html entities

---
+++ 100DaysOfCode-Python/DAY34-trivia_quiz/1_unescaping_html_entities.py
@@ -0,0 +1,10 @@
+# we saw earlier i.e., when we got the api response there was an issue -> certain special characters such as ' are not being properly shown. 
+# They r shown in the way they represent those characters i.e., ascii code or something...
+
+'''
+These r html character entities.
+They r writtem so that browser does not get confues about them
+
+< : can be part of html code so it replaces it with certain code
+" : can be part of html code so it replaces it with certain code
+'''
